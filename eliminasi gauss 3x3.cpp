ffa41e9dc9b11a7f4aedb130c9802abaef33440e
faishalhmg <43616068+faishalhmg@users.noreply.github.com>
2020-03-26 15:29:06 +0700

    Update eliminasi gauss 3x3.cpp

--- eliminasi gauss 3x3.cpp
+++ eliminasi gauss 3x3.cpp
@@ -1,40 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-	float a[3][3];
-    float b[3];
-    float (*pa)[3] = a;
-    float *pb = b;
-    float x,y,z;
-    int *angka;
-	char s;
-    
+
+ 	float x,y,z,m,n,o;
+    float a[3][3], b[3];
+    float (*pa)[3] = a, *pb = b, *cari;
+    int *angka,pilihan;
+
 void find(float *cari);
 void input(int *angka)
 {
+    cout << "\tmohon menginput data menggunakan angka!" << endl << endl;
 	cout << "Masukan Angka" << endl;
     for(int i=0;i<3;i++)
     {
         for(int j=0;j<3;j++)
-        {	
+        {
             cout << "["<<i+1<<"]" << "["<<j+1<<"]" << " = ";
-            cin >> *(*(pa +i)+j);    
-        }
-    }	
-}
+            cin >> *(*(pa +i)+j);
 
-int main()
-{
-   	input(angka);
+        }
+    }
     cout << endl << "Matriks" << endl;
     for(int i=0;i<3;i++)
     {
         for(int j=0;j<3;j++)
         {
             cout << *(*(pa +i)+j) << "\t";
-            if (j%2 == 0 && j!=0)
+            if (j==2)
                 cout << "= x" << i+1 <<endl;
         }
     }
+
     cout << endl << "Masukan Angka" << endl;
     for (int i=0;i<3;i++)
     {
@@ -42,22 +38,41 @@ int main()
         cin >> *(pb +i);
     }
 
-    x = *(*(pa +1))/ **pa;
+    persamaan:
+    cout << endl << "Persamaan Linear" << endl;
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            if(*(*(pa +i)+j)>0&&j!=0)
+                cout << "+";
+            cout << *(*(pa +i)+j);
+            if(j==0)
+                cout << "x";
+            if(j==1)
+                cout << "y";
+            if(j==2)
+                cout << "z = ";
+        }
+        cout << *(pb +i) << endl;
+    }
 
+    x = *(*(pa +1))/ **pa;
+    *(pb +1) = *(pb +1) - (x * *(pb));
     for(int i=0;i<3;i++)
     {
         *(*(pa +1)+i) = *(*(pa +1)+i) - (x * *(*(pa)+i));
     }
 
     y = *(*(pa +2))/ **pa;
-
+    *(pb +2) = *(pb +2) - (y * *(pb));
     for(int i=0;i<3;i++)
     {
         *(*(pa +2)+i) = *(*(pa +2)+i) - (y * *(*(pa)+i));
     }
 
     z = *(*(pa +2)+1)/ *(*(pa +1)+1);
-
+    *(pb +2) = *(pb +2) - (z * *(pb +1));
     for(int i=0;i<3;i++)
     {
         *(*(pa +2)+i) = *(*(pa +2)+i) - (z * *(*(pa +1)+i));
@@ -69,20 +84,47 @@ int main()
         for(int j=0;j<3;j++)
         {
             cout << *(*(pa +i)+j) << "\t";
-            if (j%2 == 0 && j!=0)
+            if (j==2)
                 cout << "= " << *(pb +i) <<endl;
         }
     }
-    find(*a);
+
+    m = *(pb +2) / (*(*(pa+2)) + *(*(pa+2)+1) + *(*(pa+2)+2));
+    n = (*(pb +1) - (*(*(pa+1)) + *(*(pa+1)+2) * m)) / *(*(pa+1)+1);
+    o = (*(pb) - (*(*(pa)+1) * n + *(*(pa)+2) * m)) / *(*pa);
+
+    cout << endl << "Hasil" << endl;
+    cout << "x = " << o << endl;
+    cout << "y = " << n << endl;
+    cout << "z = " << m << endl;
+    find(*pa);
+    return find(cari);
+}
+int main()
+{
+    cout << "\tMencari Nilai dari Persamaan Linear 3 Variabel Menggunakan Metode Gauss" << endl;
+    cout << "\t-----------------------------------------------------------------------" << endl;
+	input(angka);
     return 0;
 }
-
 void find(float *cari)
 {
-	if(*cari == s){	
-	system("cls");
-	cout<<"Input Anda salah, Masukan Angka"<<endl;
-	system("pause");
+	cout<< endl << "Ingin Ulagi ? \n1.Ya \n2.Tidak"<<endl<< "= ";
+	cin>>pilihan;
+	switch(pilihan)
+	{
+		case 1:
+		system("cls");
+		return input(angka);
+		break;
+		exit:
+    	system("cls");
+    	cout<<"Okay"<<endl;
+		case 2:
+		goto exit;
+		break;
+	}
 	cari++;
-}
+	system("cls");
+	cout<<"Input Anda salah"<<endl;
 }
